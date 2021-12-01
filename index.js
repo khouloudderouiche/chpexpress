01e5997c8720b5be26fc897ef6a6096d4ad2a9f3
khouloud <khouloudderouiche2021@gmail.com>
2021-12-01 11:43:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require ('express')
+const app = express()
+const logger = require('./middleware/logger')
+
+
+
+
+const port=5000
+app.listen(5000,()  => {
+  console.log(`the server is running on port `,5000)}
+   
+);
+app.use(express.static(__dirname + '/views'));
+app.set('view engine', 'ejs')
+
+
+
+app.use(logger)
+
+app.get('/', (req, res) => {
+    res.render('home', { title: 'home' })
+})
+app.get('/services', (req, res) => {
+    res.render('services', { title: 'services' })
+})
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: 'contact' })
+})
+app.use((req, res) => { res.status(404).render('404', { title: 'Not Found' }) })
